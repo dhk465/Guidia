ab1cd7143eeb6a2257186f1832f63141f276f0a9
Ivan Ivanov <Basurman@localhost.localdomain>
2019-11-10 11:47:06 +0100

    added lines for using libraries

--- src/ica/core.clj
+++ src/ica/core.clj
@@ -1,6 +1,16 @@
 (ns ica1.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(use 'clojure.pprint) 
+(use 'opennlp.nlp)
+(use 'opennlp.treebank) 
+(def get-sentences (make-sentence-detector "models/en-sent.bin"))
+(def tokenize (make-tokenizer "models/en-token.bin"))
+(def detokenize (make-detokenizer "models/english-detokenizer.xml"))
+(def pos-tag (make-pos-tagger "models/en-pos-maxent.bin"))
+(def name-find (make-name-finder "models/namefind/en-ner-person.bin"))
+(def chunker (make-treebank-chunker "models/en-chunker.bin"))
+
+(defn ask-tokenize [question]
+  (println question)
+  (tokenize (read-line)))
+
