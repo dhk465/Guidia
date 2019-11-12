2d1f2678904fc7d886826e0e7d27cc88f3e7f234
Ivan Ivanov <Basurman@localhost.localdomain>
2019-11-12 13:17:34 +0100

    deleted namefinder as it is unnecessary in the project scope

--- src/ica/core.clj
+++ src/ica/core.clj
@@ -7,12 +7,11 @@
 (def tokenize (make-tokenizer "models/en-token.bin"))
 (def detokenize (make-detokenizer "models/english-detokenizer.xml"))
 (def pos-tag (make-pos-tagger "models/en-pos-maxent.bin"))
-(def name-find (make-name-finder "models/namefind/en-ner-person.bin"))
 (def chunker (make-treebank-chunker "models/en-chunker.bin"))
-
-(defn -main[& args]
-(println "Lmao, main"))
 
 (defn ask-tokenize [question]
   (println question)
   (tokenize (read-line)))
+
+(defn -main[& args]
+  (ask-tokenize "How've you been?"))
