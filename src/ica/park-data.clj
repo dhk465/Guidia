d2283bcd392af2a7a823b670e176f18ec2d67491
Ivan Ivanov <Basurman@localhost.localdomain>
2019-11-15 11:48:23 +0100

    if this comment is applied, the typos will be fixed, WC deleted since it is not needed

--- src/ica/park-data.clj
+++ src/ica/park-data.clj
@@ -1,42 +1,42 @@
 
-(defrecord Park [name dogs bicycle skating refreshment sport-fields playground architecture parking restroom]) 
+(defrecord Park [name dogs bicycle skating refreshment sport-fields playground architecture parking]) 
 (def lst-park [])
-;;                       name     dogs cycl sktng refr sfied pgr arch prkg WC
-(def Bertramka (Park. "Bertramka" nil true false true false nil nil true true ))
+;;                       name     dogs cycl sktng refr sfied pgr arch prkg 
+(def Bertramka (Park. "Bertramka" nil true false true false nil nil true))
 (def lst-park (conj lst-park Bertramka ))
-;;                                                name     dogs cycl sktng  refr sfied pgr arch prkg WC
-(def Frantiskanska-zahrada (Park. "Františkánská zahrada" false true false true false true nil false true))
+;;                                                name     dogs cycl sktng  refr sfied pgr arch prkg 
+(def Frantiskanska-zahrada (Park. "Františkánská zahrada" false true false true false true nil false))
 (def lst-park (conj lst-park Frantiskanska-zahrada ))
-;;                              name    dogs cycl sktng refr sfied pgr arch prkg WC
-(def Obora-hvezda (Park. "Obora Hvězda" nil true  false  true false true nil  true true))
+;;                              name    dogs cycl sktng refr sfied pgr arch prkg 
+(def Obora-hvezda (Park. "Obora Hvězda" nil true  false  true false true nil  true))
 (def lst-park (conj lst-park Obora-hvezda))
-;;                 name   dogs cycl sktng refr sfied pgr arch prkg WC
-(def Kampa (Park. "Kampa" nil true  true  true false true nil  false true))
+;;                 name   dogs cycl sktng refr sfied pgr arch prkg 
+(def Kampa (Park. "Kampa" nil true  true  true false true nil  false))
 (def lst-park (conj lst-park Kampa))
-;;                                       name   dogs cycl sktng refr sfied pgr arch prkg WC
-(def Kinskeho-zahrada (Park. "Kínského zahrada" nil false false true false true nil false true))
-(def lst-park (conj lst-park Kampa))
-;;                       name   dogs cycl sktng refr sfied pgr arch prkg WC
-(def Klamovka (Park. "Klamovka" nil false false true false true nil true true))
-(def lst-park (conj lst-park Kampa))
-;;                         name   dogs cycl sktng refr sfied pgr arch prkg WC
-(def Landronka (Park. "Landronka" nil false true  true false true nil true true))
-(def lst-park (conj lst-park Kampa))
-;;                  name dogs  cycl sktng refr sfied pgr arch prkg WC
-(def Letna (Park. "Letná" nil false true true false true nil true true))
+;;                                       name   dogs cycl sktng refr spfld pgr arch prkg 
+(def Kinskeho-zahrada (Park. "Kínského zahrada" nil false false true false true nil false))
+(def lst-park (conj lst-park Kinskeho-zahrada))
+;;                       name   dogs cycl sktng refr sfied pgr arch prkg 
+(def Klamovka (Park. "Klamovka" nil false false true false true nil true))
+(def lst-park (conj lst-park Klamovka))
+;;                         name   dogs cycl sktng refr sfied pgr arch prkg 
+(def Landronka (Park. "Landronka" nil false true  true false true nil true))
+(def lst-park (conj lst-park Landronka))
+;;                  name dogs  cycl sktng refr sfied pgr arch prkg 
+(def Letna (Park. "Letná" nil false true true false true nil true))
 (def lst-park (conj lst-park Letna))
-;;                  name    dogs cycl sktng refr sfied pgr arch prkg WC
-(def Petrin (Park. "Petřín" nil  true  true true false true nil true true))
+;;                  name    dogs cycl sktng refr sfied pgr arch prkg 
+(def Petrin (Park. "Petřín" nil  true  true true false true nil true ))
 (def lst-park (conj lst-park Petrin))
-;;                                  name    dogs cycl sktng refr sfied pgr arch prkg WC
-(def Reigerovy-sady (Park. "Reigerovy sady" true  true  true true true true nil false true))
-(def lst-park (conj lst-park Letna))
-;;                        name    dogs cycl sktng refr sfied pgr arch prkg WC
-(def Stromovka (Park. "Stromovka" nil  true  true true false true nil true true))
+;;                                  name    dogs cycl sktng refr sfied pgr arch prkg 
+(def Reigerovy-sady (Park. "Reigerovy sady" true  true  true true true true nil false))
+(def lst-park (conj lst-park Reigerovy-sady))
+;;                        name    dogs cycl sktng refr sfied pgr arch prkg 
+(def Stromovka (Park. "Stromovka" nil  true  true true false true nil true))
 (def lst-park (conj lst-park Stromovka))
-;;                  name    dogs cycl sktng refr sfied pgr arch prkg WC
-(def Vysehrad (Park. "Vyšehrad" nil true false true false true nil true true))
+;;                  name    dogs cycl sktng refr sfied pgr arch prkg 
+(def Vysehrad (Park. "Vyšehrad" nil true false true false true nil true ))
 (def lst-park (conj lst-park Vysehrad))
-;;                                name    dogs cycl sktng refr sfied pgr arch prkg WC
-(def Vojanovy-sady (Park. "Vojanovy sady" nil  nil  nil   nil  nil   nil  nil nil  nil))
+;;                                name    dogs cycl sktng refr sfied pgr arch prkg 
+(def Vojanovy-sady (Park. "Vojanovy sady" nil  nil  nil   nil  nil   nil  nil nil))
 (def lst-park (conj lst-park Vojanovy-sady))
