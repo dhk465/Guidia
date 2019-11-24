fefde811384f9d0cce61a2ca134fc7ff36845f93
Daehee <daehee.kim465@gmail.com>
2019-11-24 15:54:33 +0100

    gives recognize function an option to return keys of the json instead of values

--- src/ica/suggest.clj
+++ src/ica/suggest.clj
@@ -11,10 +11,13 @@
   "It parses the strings from reg_phrases.json into a hashmap that is used to recognize certain keywords from the chat."
   (first (cheshire/parsed-seq (io/reader "src/ica/recog_phrases.json") true)))
 
-(defn recognize [sentence]
-  "It reads a string and checks if there is anything that matches the words in recog_phrases.json.
-  It returns a list of matched words."
-  (for [phrase (tokenize sentence)
-        feature (keys recogs)
-        :when (some #(when (or (= phrase %) (= phrase (pluralize %))) %) (get recogs feature))]
-        phrase))
+(defn recognize [sentence & args]
+  "It reads a string and checks if there is anything in the string that matches
+  the words in recog_phrases.json. It returns a list of matched words.
+  If an additional argument (keys) is given, it returns a list of keys instead."
+    (for [phrase (tokenize sentence)
+          feature (keys recogs)
+          :when (some #(when (or (= phrase %) (= phrase (pluralize %))) %) (get recogs feature))]
+          (if (apply #(= % keys) args)
+            feature
+            phrase)))
