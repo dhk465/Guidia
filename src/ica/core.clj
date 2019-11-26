3305aeff74ca3bfc9a2d6c6cba3694a16e7da7eb
ilyassakhanov <ilyas.sakhanov@gmail.com>
2019-11-26 15:11:26 +0100

    fixes indentation in data-comparer functions

--- src/ica/core.clj
+++ src/ica/core.clj
@@ -38,8 +38,7 @@
 (defn data-comparer-helper-1 [position park-stored input-park]
   "It takes a data about park and compares to data inputed by a user for one parameter."
    (= ((nth (rest (keys Bertramka )) position) input-park) 
-    ((nth (rest (keys Bertramka )) position) park-stored))
-  )
+    ((nth (rest (keys Bertramka )) position) park-stored)))
 
  (defn data-comparer-helper-2 [position input-lst-park input-park]
   "It takes in a list of parks, user inputted data and a parameter, 
@@ -49,14 +48,9 @@
     (loop [curr-parks input-lst-park]
       (when-not  (empty? curr-parks)
        (if  (= (data-comparer-helper-1 position (first curr-parks) input-park) true)
-        (var-set lst-curr-park (conj @lst-curr-park (first curr-parks)))
-       )
-    (recur (rest curr-parks))
-      )
-    )
-   (var-get lst-curr-park)
-  )
- )
+        (var-set lst-curr-park (conj @lst-curr-park (first curr-parks))))
+       (recur (rest curr-parks))))
+   (var-get lst-curr-park)))
 
  (defn data-comparer-main [lst-park input-park]
   "It takes a vector with the data about parks, a record that was created from a user input,
@@ -65,12 +59,8 @@
    (loop [position 0]
     (when (<= position 7 )
      (var-set loc-lst-park (data-comparer-helper-2 position @loc-lst-park input-park))
-     (recur (+ position 1))
-    )
-   )
-   (var-get loc-lst-park)
-  )
- )
+     (recur (+ position 1))))
+   (var-get loc-lst-park)))
 
 ; to-do: main with (recognize) fn
 (defn -main [& args]
